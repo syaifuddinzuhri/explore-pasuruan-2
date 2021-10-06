ca93b90f1e7bfddcbae11211e734c8ec90e923bb
Syaifuddin <mochammadsyaifuddinzuhri@gmail.com>
2021-10-06 17:50:14 +0700

    Create profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:miniproject/constants/color_constant.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mBackgroundColor,
+        title: SvgPicture.asset('assets/svg/explore_pasuruan_logo.svg'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Text('Halaman Profile'),
+      ),
+    );
+  }
+}
