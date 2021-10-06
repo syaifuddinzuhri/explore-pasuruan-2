82f7e8b626b9c18bd4dbb8b1247e9eb0de934225
Syaifuddin <mochammadsyaifuddinzuhri@gmail.com>
2021-10-06 17:49:39 +0700

    Add bottom navigation bar

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:miniproject/screens/search_screen.dart';
+import 'package:miniproject/screens/home_screen.dart';
+import 'package:miniproject/screens/profile_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    SearchScreen(),
+    ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false, // <-- HERE
+        showUnselectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined),
+              activeIcon: Icon(Icons.search),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: CircleAvatar(
+                radius: 11.0,
+                backgroundImage: AssetImage("assets/images/avatar.jpg"),
+              ),
+              // icon: Icon(Icons.person_add_alt_1_outlined),
+              activeIcon: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(500),
+                      border: Border.all(color: Colors.blue)),
+                  child: CircleAvatar(
+                      radius: 11.0,
+                      backgroundImage: AssetImage("assets/images/avatar.jpg"))),
+              label: '')
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+}
