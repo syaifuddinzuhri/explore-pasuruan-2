fb2b1d386957a56c9acbcc20fd2b54d62e5e8197
Syaifuddin <mochammadsyaifuddinzuhri@gmail.com>
2021-10-06 17:49:07 +0700

    Add style contstant to constant folder

---
+++ lib/constants/style_constant.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:miniproject/constants/color_constant.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// Style for title
+var mTitleStyle = GoogleFonts.inter(
+    fontWeight: FontWeight.w600, color: mTitleColor, fontSize: 16);
+
+// Style for Service Section
+var mServiceTitleStyle = GoogleFonts.inter(
+    fontWeight: FontWeight.w500, fontSize: 12, color: mTitleColor);
+var mServiceSubtitleStyle = GoogleFonts.inter(
+    fontWeight: FontWeight.w400, fontSize: 10, color: mSubtitleColor);
+
+// Style for Card Section
+var mCardTitleStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w700,
+  fontSize: 16,
+  color: mCardTitleColor,
+);
+var mCardSubtitleStyle = GoogleFonts.inter(
+  fontWeight: FontWeight.w500,
+  fontSize: 10,
+  color: mCardSubtitleColor,
+);
+
+// Style for List Section
+var mListTitleStyle = GoogleFonts.inter(
+    fontSize: 14, fontWeight: FontWeight.w900, color: mFillColor);
+var mListContentStyle = GoogleFonts.inter(
+    fontSize: 10, fontWeight: FontWeight.w500, color: mTitleColor);
+var mListPlaceStyle = GoogleFonts.inter(
+    fontSize: 10, fontWeight: FontWeight.w500, color: mBlueColor);
