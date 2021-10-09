d5da0630b1953958ff3293479a0c6bc86da0f3e9
Mochammad Syaifuddin Zuhri <1941720013@student.polinema.ac.id>
2021-10-09 23:01:41 +0700

    update carousel model

--- lib/models/carousel_model.dart
+++ lib/models/carousel_model.dart
@@ -9,6 +9,7 @@ List<CarouselModel> carousels = carouselsData
     .toList();
 
 var carouselsData = [
+  {"image": "assets/images/matanai.jpg"},
   {"image": "assets/images/tsi.jpg"},
   {"image": "assets/images/cempaka.jpg"},
   {"image": "assets/images/jendelalangit.jpg"},
