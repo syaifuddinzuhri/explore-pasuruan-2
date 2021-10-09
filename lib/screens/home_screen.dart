32abfb4a2096d50bd527c5e3f47ff5004f05c9ee
Mochammad Syaifuddin Zuhri <1941720013@student.polinema.ac.id>
2021-10-09 20:21:14 +0700

    Add carousel or slider using swiper library

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
 import 'package:miniproject/constants/color_constant.dart';
+import 'package:miniproject/models/carousel_model.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,16 +15,57 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mBackgroundColor,
-        title: Container(
-          width: 110,
-          child: Image.asset("assets/images/logo.png", fit: BoxFit.fill)),
-        elevation: 0,
-      ),
-      body: Center(
-        child: Text('Halaman Home'),
-      ),
-    );
+        appBar: AppBar(
+          backgroundColor: mBackgroundColor,
+          title: Container(
+              width: 110,
+              child: Image.asset("assets/images/logo.png", fit: BoxFit.fill)),
+          elevation: 0,
+        ),
+        body: Container(
+          margin: EdgeInsets.only(left: 16, right: 16),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 16),
+                  alignment: Alignment.centerLeft,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 190,
+                        child: Swiper(
+                          onIndexChanged: (index) {},
+                          autoplay: true,
+                          layout: SwiperLayout.DEFAULT,
+                          viewportFraction: 0.8,
+                          scale: 0.9,
+                          itemCount: carousels.length,
+                          pagination:
+                              SwiperPagination(margin: EdgeInsets.all(5.0)),
+                          itemBuilder: (BuildContext context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                    image: AssetImage(carousels[index].image),
+                                    fit: BoxFit.fill),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
