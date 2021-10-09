0defb39b07e0b76e8272e359f77fa20873ba8906
Mochammad Syaifuddin Zuhri <1941720013@student.polinema.ac.id>
2021-10-10 00:57:33 +0700

    create detail tour screen

---
+++ lib/screens/detail_tour.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class DetailTourScreen extends StatelessWidget {
+  final int index;
+
+  DetailTourScreen(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Text('Hello'));
+  }
+}
